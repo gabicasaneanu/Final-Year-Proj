34425e0ad6ac9311318bd8f74331c70a16c0d25b
gabicasaneanu <66840524+gabicasaneanu@users.noreply.github.com>
2023-12-30 23:12:29 +0000

    Update nmapjuice.py

--- nmapjuice.py
+++ nmapjuice.py
@@ -2,7 +2,9 @@ import subprocess
 import netifaces
 import os
 import nmap
-import pprint
+import json
+import re
+from packaging import version
 
 def writeHostAddress():
     x = netifaces.interfaces()
@@ -44,13 +46,79 @@ for proto in protos:
     
    
 Vars = {}
-
+Titles = []
 
 
 a = list(nmScan.values())
 x = a[1]
 for ports in portx:
     Vars[x[victim]['tcp'][ports]['product']] = x[victim]['tcp'][ports]['version']
+    
 
 
-print (Vars)
+for i in Vars:
+    output = subprocess.run(['sudo','searchsploit',i,'-j'], capture_output = True).stdout
+    logput = json.loads(output)
+    pogput = list(logput.values())
+    titles = pogput[2]
+    versione = Vars[i].split()
+    versionel = versione[0]
+    for title in titles:
+        TitleVers = title['Title']
+        for snow in TitleVers.split():
+            try: 
+                pawg = version.Version(snow)
+                if (version.parse(versionel) >= pawg) == True:
+                    print('exploit for:',i,'Version:',versionel,'\n-',title['Title'],title['Path'])
+                    
+                
+            except:
+                pass
+            
+            
+        
+        
+
+
+
+
+
+
+
+
+    
+
+
+#pompem
+
+
+
+
+'''
+for host in nm.all_hosts():
+     for proto in nm[host].all_protocols():
+         print('----------')
+         print('Protocol : %s' % proto)
+
+         lport = nm[host][proto].keys()
+         sorted(lport)
+         for port in lport:
+             print ((port, nm[host][proto][port]['state']))
+'''
+
+                
+            
+
+
+            
+            
+
+
+
+    
+    
+
+    
+    
+    
+        
