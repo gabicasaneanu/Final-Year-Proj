87ff43f42f683ad14472a30627ecfb5a49cc1c68
gabicasaneanu <66840524+gabicasaneanu@users.noreply.github.com>
2024-03-05 15:27:33 +0000

    Update nmapjuice.py

--- nmapjuice.py
+++ nmapjuice.py
@@ -4,9 +4,8 @@ import os
 import nmap
 import json
 import re
-from pkg_resources import packaging
-from pkg_resources import parse_version as version
-
+from itertools import chain
+from pprint import pprint
 def writeHostAddress():
     x = netifaces.interfaces()
     x.pop(0)
@@ -29,73 +28,65 @@ def writeHostAddress():
 host = writeHostAddress()
 #print netifaces.interfaces()
 interface = 'tun0'
-victim = '10.10.11.233'
+victim = '10.10.11.242'
 
         
 
+tries = 3
+for i in range(tries):
+    try:
+        nm = nmap.PortScanner()
+        nmScan = nm.scan(hosts=victim, arguments= '-sV -T4')
+    except:
+        if i < tries - 1:
+            continue
+        else:
+            raise Exception('nmap scan failed - host may to be up, check host       connectivity / IP (Run Again, nmap may fail on occasion')
+    break
 
 
-nm = nmap.PortScanner()
-nmScan = nm.scan(hosts=victim, arguments= '-sV -T4')
-
-protos = nm[victim].all_protocols()    
-for proto in protos:
-    ports = nm[victim][proto].keys()
-    portx = list(ports)
+try:
+    protos = nm[victim].all_protocols()    
+    for proto in protos:
+        ports = nm[victim][proto].keys()
+        portx = list(ports)
+except:
+    print('nmap scan failed, try running again')
+    quit()
     
        
     
    
-Vars = {}
+Vars = []
 Titles = []
 
 
 a = list(nmScan.values())
 x = a[1]
 for ports in portx:
-    Vars[x[victim]['tcp'][ports]['product']] = x[victim]['tcp'][ports]['version']
-    
-print(Vars)
-thispass = '' 
-for i in Vars:
-    if not i:
-        continue
-    gong = i.split()
-    act = gong[0]
-    if re.match("^[A-Za-z_-]+$",act):
-        output = subprocess.run(['sudo','searchsploit',act,'-j'], capture_output = True).stdout
-        logput = json.loads(output)
-        pogput = list(logput.values())
-        titles = pogput[2]
-        versione = Vars[i].split()
-        versionel = versione[0]
-        print('\n','exploits for:',act,'-',versionel)
-        for title in titles:
-            TitleVers = title['Title']
-            print(title['Title'],title['Path'])
-            
-           
-                  
-                
-                    
-                       
-            
-        
-        
+    Vars.append([x[victim]['tcp'][ports]['product']])
+    Titles.append([x[victim]['tcp'][ports]['version']])
 
-
-
-
-
+varsx = list(chain.from_iterable(Vars))    
+varsxx = list(filter(None,varsx))
 
 
 
     
-
-
-#pompem
-
-
+CVEs = {}
+for i,x  in enumerate(varsxx):
+    print('services found : ',x, 'version - ', Titles[i])
+    output = subprocess.run(['sudo','searchsploit',x,'-j'], capture_output = True).stdout
+    logput = json.loads(output)
+    pogput = list(logput.values())
+    titles = pogput[2]
+    somde = titles[-7:]
+    for som in somde:
+        print(som['Title'],som['Codes'])
+        CVEs[som['Title']] = som['Codes'] 
+          
+         
+               
 
 
 
