48306ba4dfa19114a657fee7538e2cb6011ba1cb
gabicasaneanu <66840524+gabicasaneanu@users.noreply.github.com>
2024-03-09 02:56:29 +0000

    Update packetfilter.py

--- packetfilter.py
+++ packetfilter.py
@@ -2,7 +2,6 @@ import subprocess
 import json
 import sys
 
-
 victim = sys.argv[1]
 
 #run siege for 10seconds, output in json format
@@ -17,11 +16,11 @@ success = float(parseg['successful_transactions'])
 
 
 if parseg['transactions'] == parseg['successful_transactions']:
-    print('no packet filtering measures in place - configure packet filtering and/or nacl')
-    print('the transaction rate was:', parseg['transaction_rate'])
-    print('the availability was:',parseg['availability'])
+    print('no packet filtering measures in place - configure packet filtering and/or nacl <br/>')
+    print('the transaction rate was:', parseg['transaction_rate'],'<br/>')
+    print('the availability was:',parseg['availability'],'<br/>')
     quit()
 elif success >= (transactamount*0.80):
-    print('success rate seems to be equal or more than 80% - packet filter may be misconfigured')
+    print('success rate seems to be equal or more than 80% - packet filter may be misconfigured or not in place')
 else:
     print('success rate is lower than 80% - packet filtering seems to be working for >= 80% of packets')
